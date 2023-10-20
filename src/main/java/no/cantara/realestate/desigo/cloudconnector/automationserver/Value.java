593291c45193c51ec98e17c58fa8b30bfff52d73
baardl <bard.lind@gmail.com>
2023-10-20 15:55:57 +0200

    #10 Parse PresentValue from Desigo

--- src/main/java/no/cantara/realestate/desigo/cloudconnector/automationserver/Value.java
+++ src/main/java/no/cantara/realestate/desigo/cloudconnector/automationserver/Value.java
@@ -1,12 +1,29 @@
 package no.cantara.realestate.desigo.cloudconnector.automationserver;
 
-import javax.json.bind.annotation.JsonbProperty;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.time.Instant;
+import java.util.Objects;
 
 public class Value {
-    @JsonbProperty("value.value")
+    /*
+    "DataType": "BasicFloat",
+    "Value": {
+      "Value": "587",
+      "Quality": "9439544818976425217",
+      "QualityGood": true,
+      "Timestamp": "2023-10-17T13:43:57.153Z"
+    }
+     */
+    @JsonProperty("Value")
     private Number value;
-    @JsonbProperty("value.units")
-    private String units;
+    @JsonProperty("Timestamp")
+    private Instant sampleDate;
+
+    @JsonProperty("QualityGood")
+    private Boolean isReliable;
+
+
 
     public Value() {
 
@@ -20,20 +37,41 @@ public class Value {
         this.value = value;
     }
 
-
-    public String getUnits() {
-        return units;
+    public Instant getSampleDate() {
+        return sampleDate;
     }
 
-    public void setUnits(String units) {
-        this.units = units;
+    public void setSampleDate(Instant sampleDate) {
+        this.sampleDate = sampleDate;
+    }
+
+    public Boolean getIsReliable() {
+        return isReliable;
+    }
+
+    public void setIsReliable(Boolean isReliable) {
+        this.isReliable = isReliable;
     }
 
     @Override
     public String toString() {
         return "Value{" +
                 "value=" + value +
-                ", units='" + units + '\'' +
+                ", sampleDate=" + sampleDate +
+                ", isReliable=" + isReliable +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Value value1 = (Value) o;
+        return Objects.equals(getValue(), value1.getValue()) && Objects.equals(getSampleDate(), value1.getSampleDate()) && Objects.equals(getIsReliable(), value1.getIsReliable());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getValue(), getSampleDate(), getIsReliable());
     }
 }
