e912cad9a84a0129e1c8b3285f6c64862cee586f
baardl <bard.lind@gmail.com>
2023-10-20 10:50:23 +0200

    #10 Add debug info

--- src/main/java/no/cantara/realestate/desigo/cloudconnector/automationserver/DesigoApiClientRest.java
+++ src/main/java/no/cantara/realestate/desigo/cloudconnector/automationserver/DesigoApiClientRest.java
@@ -62,6 +62,15 @@ public class DesigoApiClientRest implements SdClient {
         ApplicationProperties config = new DesigoCloudconnectorApplicationFactory()
                 .conventions(ApplicationProperties.builder())
                 .buildAndSetStaticSingleton();
+        java.util.logging.Logger.getLogger("org.apache.http.wire").setLevel(java.util.logging.Level.FINEST);
+        java.util.logging.Logger.getLogger("org.apache.http.headers").setLevel(java.util.logging.Level.FINEST);
+
+        System.setProperty("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.SimpleLog");
+        System.setProperty("org.apache.commons.logging.simplelog.showdatetime", "true");
+        System.setProperty("org.apache.commons.logging.simplelog.log.httpclient.wire", "debug");
+        System.setProperty("org.apache.commons.logging.simplelog.log.org.apache.http", "debug");
+        System.setProperty("org.apache.commons.logging.simplelog.log.org.apache.http.headers", "debug");
+
         String trendId = getConfigValue("trend.id");
         String apiUrl = getConfigValue("sd.api.url");
         URI apiUri = new URI(apiUrl);
