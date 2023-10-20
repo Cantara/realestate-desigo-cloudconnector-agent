585d2ed5d1638e32072f084e49f80e4bccfdde0c
baardl <bard.lind@gmail.com>
2023-10-20 10:25:35 +0200

    Removed unused code.

--- src/main/java/no/cantara/realestate/desigo/cloudconnector/automationserver/DesigoApiClientRest.java
+++ src/main/java/no/cantara/realestate/desigo/cloudconnector/automationserver/DesigoApiClientRest.java
@@ -49,28 +49,13 @@ public class DesigoApiClientRest implements SdClient {
     private static final String LATEST_BY_DATE = "SampleDateDescending";
     private final NotificationService notificationService;
     private UserToken userToken = null;
-//    private final DesigoApiLogonService logonService;
     private long numberOfTrendSamplesReceived = 0;
     private boolean isHealthy = true;
 
     public DesigoApiClientRest(URI apiUri, NotificationService notificationService) {
         this.apiUri = apiUri;
-//        logonService = null;
-        this.notificationService = notificationService;
-        /*RestClientBuilder.newBuilder()
-                .baseUri(apiUri)
-                .build(DesigoApiLogonService.class);
-
-         */
-    }
-
-    /*
-    protected DesigoApiClientRest(URI apiUri, DesigoApiLogonService logonService, NotificationService notificationService) {
-        this.apiUri = apiUri;
-        this.logonService = logonService;
         this.notificationService = notificationService;
     }
-    */
 
     public static void main(String[] args) throws URISyntaxException, SdLogonFailedException {
 
--- src/main/java/no/cantara/realestate/desigo/cloudconnector/automationserver/DesigoApiLogonService.java
+++
@@ -1,6 +0,0 @@
-package no.cantara.realestate.desigo.cloudconnector.automationserver;
-
-@Deprecated
-public interface DesigoApiLogonService {
-    UserToken logon(String jsonBody);
-}
